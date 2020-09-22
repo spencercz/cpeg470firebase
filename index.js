f9237dfe79715a964aa9604f5efc911062f9c5af
spencercz <48261199+spencercz@users.noreply.github.com>
2020-09-22 15:40:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
-// Initialize Firebase
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBc4YPodSOW5uFSJ8GtlX4YHceT8vUAbas",
-    authDomain: "cpeg470-spencer-game.firebaseapp.com",
-    databaseURL: "https://cpeg470-spencer-game.firebaseio.com",
-    projectId: "cpeg470-spencer-game",
-    storageBucket: "cpeg470-spencer-game.appspot.com",
-    messagingSenderId: "209011549042",
-    appId: "1:209011549042:web:dbfbae09cc0c09035373eb"
-  };
-var google_provider = new firebase.auth.GoogleAuthProvider();
-firebase.initializeApp(firebaseConfig);
+$(document).ready(function(){
+    alert("hi");
+    // Initialize Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyBc4YPodSOW5uFSJ8GtlX4YHceT8vUAbas",
+        authDomain: "cpeg470-spencer-game.firebaseapp.com",
+        databaseURL: "https://cpeg470-spencer-game.firebaseio.com",
+        projectId: "cpeg470-spencer-game",
+        storageBucket: "cpeg470-spencer-game.appspot.com",
+        messagingSenderId: "209011549042",
+        appId: "1:209011549042:web:dbfbae09cc0c09035373eb"
+      };
+    var google_provider = new firebase.auth.GoogleAuthProvider();
+    firebase.initializeApp(firebaseConfig);
 
-firebase.auth().onAuthStateChanged(user => {
-  if (!!user){
-    alert(`${user.displayName || user.email}`);
-  }
-});
-
-$("#loginemail").click(()=>{
-  firebase.auth().signInWithEmailAndPassword($("#email").val(), $("#password").val()).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    alert(errorMessage);
-  });
-});
-$("#register").click(()=>{
-  let pwd1 = $("#password").val();
-  let pwd2 = $("#password2").val();
-  if (pwd1 == pwd2){
-    firebase.auth().createUserWithEmailAndPassword($("#email").val(), $("#password").val()).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert(errorMessage);
+    firebase.auth().onAuthStateChanged(user => {
+      if (!!user){
+        alert(`${user.displayName || user.email}`);
+      }
     });
-  } else {
-    alert("passwords don't match");
-  }
-});
-$("#reset").click(()=>{
-  firebase.auth().sendPasswordResetEmail($("#email").val());
-  alert(`A password reset email has been sent to ${user.email}`);
-});
-$("#clear").click(()=>{
-  $("#email").val("");
-  $("#password").val("");
-  $("#password2").val("");
+
+    $("#loginemail").click(()=>{
+      firebase.auth().signInWithEmailAndPassword($("#email").val(), $("#password").val()).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+      });
+    });
+    $("#register").click(()=>{
+      let pwd1 = $("#password").val();
+      let pwd2 = $("#password2").val();
+      if (pwd1 == pwd2){
+        firebase.auth().createUserWithEmailAndPassword($("#email").val(), $("#password").val()).catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          alert(errorMessage);
+        });
+      } else {
+        alert("passwords don't match");
+      }
+    });
+    $("#reset").click(()=>{
+      firebase.auth().sendPasswordResetEmail($("#email").val());
+      alert(`A password reset email has been sent to ${user.email}`);
+    });
+    $("#clear").click(()=>{
+      $("#email").val("");
+      $("#password").val("");
+      $("#password2").val("");
+    });
 });
